93afa16680cc7baa7f898cc44af2e8cd5efdf3e1
Vincent Herrin <vinceherrin.com>
2021-05-23 22:07:04 -0500

    adding dist subtree

---
+++ precache-manifest.1b7045d6418fe64b1dcf81b9bc20d300.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f211ed87eaeef09cbe6",
+    "url": "/vue-crypto-dashboard/css/app.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vue-crypto-dashboard/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vue-crypto-dashboard/img/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "928c10d1141789acd4889f85a76728b5",
+    "url": "/vue-crypto-dashboard/index.html"
+  },
+  {
+    "revision": "5f211ed87eaeef09cbe6",
+    "url": "/vue-crypto-dashboard/js/app.js"
+  },
+  {
+    "revision": "959201ea766e478e18fc",
+    "url": "/vue-crypto-dashboard/js/chunk-00afbcf4.js"
+  },
+  {
+    "revision": "762a5b914548be27fa2a",
+    "url": "/vue-crypto-dashboard/js/chunk-453573ed.js"
+  },
+  {
+    "revision": "ffa5425b98e9ff0c9906",
+    "url": "/vue-crypto-dashboard/js/chunk-vendors.js"
+  },
+  {
+    "revision": "59b4fc26343074884653a2af117147f9",
+    "url": "/vue-crypto-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-crypto-dashboard/robots.txt"
+  }
+]);
